7cce149c84b74cee84e4aafcd6b405f1087fcd58
MustDie-green <84462376+MustDie-green@users.noreply.github.com>
2025-01-26 23:16:27 +0300

    Fix timeout error with big books

--- bot/handlers/file_handler.py
+++ bot/handlers/file_handler.py
@@ -2,7 +2,7 @@ import os
 import logging
 import io
 
-from aiogram import types, Dispatcher, F
+from aiogram import types, Dispatcher, F, Bot
 from aiogram.fsm.context import FSMContext
 
 from bot.services.mail_services import send_book_via_yandex
@@ -15,6 +15,7 @@ from config import SUPPORTED_BOOK_FORMATS
 from bot.validators import is_valid_book_format
 
 async def file_handler(message: types.Message, state: FSMContext):
+    logging.debug('Запустился файл хендлер')
     current_state = await state.get_state()
 
     file = message.document
@@ -30,20 +31,30 @@ async def file_handler(message: types.Message, state: FSMContext):
                              "Если вы хотите, чтобы бот поддерживал больше форматов, напишите автору бота или сделайте pull request — /help")
         logging.warning(f"Прислали файл неподходящего формата: {file_name}")
         return
+    else:
+        await message.answer(f"Получил файл {file_name}, сейчас отправлю, ждите новостей")
+    
 
     file_in_io = io.BytesIO()
     file_info = await message.bot.get_file(file.file_id)
-    await message.bot.download_file(file_info.file_path, file_in_io)
+    try:
+        await message.bot.download_file(file_info.file_path, file_in_io)
+    except Exception as e:
+        message.answer('Произошла ошибка при скачивании файла из Телеграм, возможно, он слишком большой для меня, напиши автору бота')
+        logging.error(f"Ошибка при скачивании файла: {e}")
+        return
+    logging.debug('Скачали файл')
 
     if current_state == BotMode.dropbox.state:
+        logging.debug('Грузим в дропбокс')
         if not await ensure_dropbox_auth(message, state):
             message.answer('Включен режим Dropbox, но вы не авторизировались в нем, нажмите /auth')
             return
-        await message.answer(f"Получил файл {file_name}, отправляем в Dropbox.")
         await upload_book_to_dropbox(file_in_io, file_name, state)
         await message.answer(f"Книга успешно загружена в Dropbox: /Приложения/Dropbox PocketBook/{file_name}")
 
     elif current_state == BotMode.email.state:
+        logging.debug('Отправляем на почту')
         user_data = get_user_data(message.from_user.id)
         if not user_data:
             await message.answer("Вы не добавили почту. Нажмите /add_email.")
--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from aiogram import Bot, Dispatcher
 from config import TELEGRAM_BOT_TOKEN  
 from bot.handlers import register_all_handlers 
 from bot.services.db_services import init_db
+from aiohttp import ClientSession, ClientTimeout
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 async def main():
     init_db()
-    
-    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+    timeout = ClientTimeout(total=150) 
+
+    bot = Bot(token=TELEGRAM_BOT_TOKEN, timeout=timeout)
     dp = Dispatcher()
     
     register_all_handlers(dp)
